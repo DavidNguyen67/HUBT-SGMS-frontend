bdb281d50ee070af4314301d2cc630959a2d50ef
QuanT4_HoangMai <davidnguyen67dev@gmail.com>
2025-05-29 03:52:21 +0700

    refactor: simplify IndexPage component by removing authentication logic and redirect handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,16 @@
 'use client';
 
-import { Suspense, useEffect } from 'react';
-import { useIsAuthenticated, useResource } from '@refinedev/core';
-import { redirect, useRouter } from 'next/navigation';
-import { authProviderClient } from '@providers/auth-provider/auth-provider.client';
-import { resources } from '@common/resources';
+import { Suspense } from 'react';
+
+import { Authenticated } from '@refinedev/core';
+import { NavigateToResource } from '@refinedev/nextjs-router';
 
 export default function IndexPage() {
-  const { data: authData } = useIsAuthenticated();
-  const router = useRouter();
-
-  useEffect(() => {
-    const redirectToFirstAccessibleResource = async () => {
-      if (authData?.authenticated) {
-        const userRole =
-          (await authProviderClient?.getPermissions?.()) as string[];
-
-        const firstMatchingResource = resources.find((resource) => {
-          const roles = resource.meta?.roles as string[] | undefined;
-          return (
-            !roles ||
-            roles.length === 0 ||
-            roles.includes(userRole?.[0] ?? userRole)
-          );
-        });
-
-        if (firstMatchingResource?.list) {
-          router.push(firstMatchingResource.list as string);
-        } else {
-          redirect('/login');
-        }
-      }
-    };
-
-    redirectToFirstAccessibleResource();
-  }, [authData?.authenticated, router]);
-
-  return <Suspense fallback={<div>Loading...</div>} />;
+  return (
+    <Suspense>
+      <Authenticated key="home-page">
+        <NavigateToResource />
+      </Authenticated>
+    </Suspense>
+  );
 }
